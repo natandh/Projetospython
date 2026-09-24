7674e763183e0c49263fe53f5289a16498f79cd9
Natan Araujo <natandh@gmail.com>
2026-09-24 16:22:54 -0300

    Sistema de emprestimo

---
+++ Sistema de empréstimo/main.py
@@ -0,0 +1,11 @@
+'''Uma biblioteca possui 100 livros disponíveis.
+Menu:
+1 - Emprestar livro
+2 - Devolver livro
+3 - Consultar quantidade disponível
+0 - Encerrar
+Ao emprestar, a quantidade disponível diminui em 1. Ao devolver, aumenta em 1.
+Não permitir empréstimo quando não houver livros nem devolução quando não houver livros
+emprestados.
+Ao finalizar, apresente quantidade de empréstimos, quantidade de devoluções e livros disponíveis.'''
+
